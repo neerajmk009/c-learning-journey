f4f3671f878c0e3868fd5db81f6af9a07629c77c
neerajmk009 <neerajmk009@gmail.com>
2026-09-12 07:10:31 +0530

    Day1 C programs

---
+++ Day1P2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+    int a,b,c,sum;
+
+    printf(" Enter Three No: ");
+    scanf("%d%d%d" ,&a,&b,&c);
+
+    sum=a+b+c;
+
+    printf(" Sum=%d ",sum);
+    
+}
---
+++ Day1P4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+
+// Hey Everyone My Name is Neraj 
+/* This is my 4 code */
+
+    printf(" Hello World ");
+}
